f86ea4a9069c3187e4f121f17c938cbeca35c632
deus6543 <deus6543@gmail.com>
2020-10-07 16:00:51 +0000

    07.10.2020

--- main.hs
+++ main.hs
@@ -6,4 +6,20 @@ triangle=[(a,b,c)|c<-[1,2..10],b<-[1,2..c],a<-[1,2..b],
 
 length'::(Num b)=>[a]->b
 length' xs =sum [1| _<-xs]
---60стр
+
+tt [x]="111"
+tt (_:_)="444"
+
+
+--replicate'::(Eq n, Show e)=>n->e->[e]
+replicate' 1 e=[e]
+replicate' 0 e=[]
+replicate' n e=e:(replicate' (n-1) e)
+
+take' n (x:xs)
+    | n<=0 =[]
+    | otherwise = x:(take'  (n-1) xs)
+
+reverse' []=[]
+
+reverse' (x:xs)=(reverse' xs)++[x]
